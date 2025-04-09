b6ce4d542074ef66530db1aaa90dea00516a827e
Edir P Santos <fennder@gmail.com>
2025-04-09 09:23:15 -0300

    Include array users in index.jsx

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,11 +1,47 @@
-import './index.css'
+import './style.css'
+import Trash from '../../assets/lixeira.png'
 
 function Home() {
 
+  const users = [
+    {
+      id: 'asdas13231as',
+      name: 'Lucas',
+      email: 'lucas@gmail.com',
+      age: 20
+    },
+    {
+      id: 'asdas1323109',
+      name: 'João',
+      email:'joao@gmail.com',
+      age: 25
+    }]
+
   return (
 
-    <div>
-      <h1>Olá, React!</h1>  
+    <div className='container'>
+      <form>
+        <h1>Cadastro de Usuários</h1>
+        <input name='name' type="text" placeholder='Enter your name' />
+        <input name='email' type="email" placeholder='Enter your email' />
+        <input name='age' type="number" placeholder='Enter your age' />
+        <button type="button" className='button'>Cadastrar</button>
+      </form>
+
+      {users.map((user) => (
+        <div key={user.id} className='card'>
+          <div>
+            <p>Nome: {user.name}</p>
+            <p>Email: {user.email}</p>
+            <p>Age: {user.age}</p>
+          </div>
+          
+          <button>
+            <img src={Trash} alt="Trash" />
+          </button>
+        </div>
+
+      ))}
     </div>      
 
   )
